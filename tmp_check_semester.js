cedfb27f7e58bee1e5626c27edd61f30208b0d36
Powxx <rossel.pascal@gmail.com>
2026-06-25 14:23:20 +0200

    fix build

---
+++ tmp_check_semester.js
@@ -0,0 +1,1 @@
+const { PrismaClient } = require('@prisma/client'); const sql = require('node:sql'); const p = new PrismaClient(); p.\\SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'Semester' ORDER BY ordinal_position\.then(r=,null,2))).catch(e=
